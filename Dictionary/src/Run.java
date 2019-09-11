90188aed318044797f728b0e2b2dc75fbc8a73be
Serkan TAŞ <tasserkan93@gmail.com>
2019-09-11 20:24:21 +0300

    Dictionary example

---
+++ Dictionary/src/Run.java
@@ -0,0 +1,12 @@
+public class Run {
+
+    public static void main(String[] args) {
+        String[] array = new String[]{
+                "Serkan",
+                "Mehmet",
+                "Ali",
+                "Sinan"
+        };
+    }
+}
+
